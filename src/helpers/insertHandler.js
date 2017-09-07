1210f7a113f4def7513caaaaf1e5eab66712c6f3
Michiel Cuvelier <cuvelier.michiel@gmail.com>
2017-09-07 13:19:55 +0200

    Added support for insert on update of edges

--- src/helpers/insertHandler.js
+++ src/helpers/insertHandler.js
@@ -9,37 +9,30 @@ import OperationTypes from '../enums/OperationTypes';
  *
  * @param db
  * @param template
- * @param rooms
  * @param insertedObject
  * @param cache
  */
 export default function insertHandler(io, db, room, roomHash, collectionType, res) {
   const id = res.content['_id'];
+  let filteredRoomQueries = [];
+  const filteredTemplates = _.filter(room.templates, (t, i) => {
+    if (_.lowerCase(t.collection) === _.lowerCase(collectionType.name)) {
+      filteredRoomQueries.push(room.queries[i]);
+      return true;
+    }
+    return false;
+  });
   // TODO: optimization, remove unused templates for performance increase
-  const resolver = new Resolver(db, room.templates, room.queries, {}, true);
-  const fields = _.flatten(_.concat(
-    _.map(
-      _.filter(room.templates, t => {
-        return _.lowerCase(t.collection) === _.lowerCase(collectionType.name);
-      }),
-      'fields',
-    ),
-  ));
+  const resolver = new Resolver(db, filteredTemplates, filteredRoomQueries, {}, true);
+  const fields = _.filter(_.flatten(_.concat(_.map(filteredTemplates, 'fields'))), f => {
+    return !!f;
+  });
 
   resolver.resolve(room.queryParams).then(result => {
-    let data = _.find(result[0].data, ['_id', id]);
-
+    let data = _.find(_.flatten(_.map(result, 'data')), ['_id', id]);
     if (data !== undefined) {
       io.sockets.adapter.rooms[roomHash].cache.push(id);
-      emitResults(
-        io,
-        roomHash,
-        room,
-        OperationTypes.INSERT,
-        collectionType.name,
-        data,
-        fields,
-      );
+      emitResults(io, roomHash, room, OperationTypes.INSERT, collectionType.name, data, _.size(fields) === 0 ? undefined : fields);
     }
   });
 }
