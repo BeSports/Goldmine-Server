1c4cfe380e6993cd7fd7b3aea7f1244d28d7fe7c
Michiel Cuvelier <cuvelier.michiel@gmail.com>
2017-09-28 16:10:00 +0200

    Expanded the top querys with extrafields property that will be merged over them

--- src/resolvers/OrientDbQueryResolver.js
+++ src/resolvers/OrientDbQueryResolver.js
@@ -108,7 +108,9 @@ export default class OrientDBQueryResolver {
           }
         }
       });
-
+      if(template.extraFields) {
+        _.merge(formattedObject, template.extraFields);
+      }
       result.push(formattedObject);
     });
 
