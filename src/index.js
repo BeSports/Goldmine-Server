2448f3af3bf185b6c81eee828603fef1d2a9713e
Xander Berkein <xander.berkein@gmail.com>
2018-02-21 16:48:25 +0100

    Add pingConnection listener to check if your connection is still live

--- src/index.js
+++ src/index.js
@@ -156,6 +156,10 @@ const startQuerries = async (Config, publications) => {
       console.log('CLIENT CONNECTED:', socket.id);
     }
 
+    socket.on('pingConnection', data => {
+      io.sockets.emit('pongConnection', data);
+    });
+
     // -----------------------------------------------------
     // -----------------------------------------------------
 
